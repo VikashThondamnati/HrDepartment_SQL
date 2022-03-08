7c29c7a19ab1be1728eab4f2876ab978585aacb2
VikashThondamnati <t.vikash2604@gmail.com>
2022-03-08 15:56:58 +0000

    new files uploaded

---
+++ Workspace2.sql
@@ -0,0 +1,13 @@
+--SPRINT 5 PROJECT WORKSPACE HR_PERFORMANCE_SET_2
+
+--Questions:
+
+--1. Find how many Employees work under 'ABE LINCOLN PRES LIBRARY MUS2'?
+
+--2. Find the Employee list who are all working as STATIONARY ENGINEER?
+
+--3. Display the Employee list who is getting YTD Gross above 50000?
+
+--4. Display the Employee list who is working as ACCOUNTANT SUPERVISOR and YTD Gross above 50000?
+
+--5. Find total Employee list work as ELECTRICIAN?
---
+++ Workspace3.sql
@@ -0,0 +1,13 @@
+--SPRINT 5 PROJECT WORKSPACE HR_PERFORMANCE_SET_3
+
+--Questions:
+
+--1. List the unique Agency Name in descending order?
+
+--2. List the employee names that work under "HEALTHCARE & FAMILY SERVICES" in descending order?
+
+--3. Find the employee count work under "HEALTHCARE & FAMILY SERVICES"?
+
+--4. Find the position of Employee CHRISTIAN MCWHIRTER?
+
+--5. Find the agency name of employee DENNIS HOLLOWAY?
